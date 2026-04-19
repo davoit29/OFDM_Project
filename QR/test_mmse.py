25b0b7e5cfcfaa882a8a059d5498ab3c30a30406
davoit29 <166952608+davoit29@users.noreply.github.com>
2026-04-19 10:03:53 +0300

    Update and rename test_mmse to test_mmse.py

---
+++ QR/test_mmse.py
@@ -0,0 +1,106 @@
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+
+class MMSETest:
+    def __init__(self, number_subcarriers, SNR):
+        self.number_subcarriers = number_subcarriers
+        self.SNR = SNR
+
+    def qr_solve(self, A, b):
+
+        r = np.sqrt((A[0, 0]) ** 2 + (A[1, 0]) ** 2)
+
+        c = A[0, 0] / r
+        s = A[1, 0] / r
+
+        G = np.array([
+            [c, s],
+            [-s, c]
+        ])
+
+
+
+        R = G @ A
+
+        y = G @ b
+
+        x_2 = y[1] / R[1, 1]
+
+        x_1 = (y[0] - R[0, 1] * x_2) / R[0, 0]
+
+        x = np.array([x_1, x_2], dtype=complex)
+
+        return x
+
+    def _mmse(self, y_tensor, H_est):
+        number_ofdm_symbols = y_tensor.shape[0]
+
+        x_est = np.zeros((number_ofdm_symbols, self.number_subcarriers, 2), dtype=complex)
+
+        for i in range(number_ofdm_symbols):
+            for k in range(self.number_subcarriers):
+                Hk = H_est[i, k, :, :]
+                yk = y_tensor[i, k, :]
+
+                Hh = Hk.conj().T
+                A = Hh @ Hk + (1 / self.SNR) * np.eye(2)
+
+                b = Hh @ yk
+
+
+
+                x_est[i, k, :] = self.qr_solve(A,b)
+
+        return x_est
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ================= ПАРАМЕТРЫ =================
+subcarriers = 7
+SNR = 10
+
+mmse = MMSETest(subcarriers, SNR)
+
+# фиксированная H (2x2)
+H_const = np.array([[1+1j, 0.5], [0.3j, 1-0.2j]])
+
+symbols_range = np.arange(10, 1000, 20)
+times = []
+
+for Nsym in symbols_range:
+    # создаем тензоры
+    y_tensor = np.random.randn(Nsym, subcarriers, 2) + 1j * np.random.randn(Nsym, subcarriers, 2)
+
+    # H тензор (копируем одну матрицу)
+    H_est = np.tile(H_const, (Nsym, subcarriers, 1, 1))
+
+    start = time.time()
+    mmse._mmse(y_tensor, H_est)
+    end = time.time()
+
+    times.append(end - start)
+
+
+# ================= ГРАФИК =================
+plt.figure()
+plt.plot(symbols_range, times, marker='o')
+plt.xlabel("Количество OFDM символов")
+plt.ylabel("Время выполнения (сек)")
+plt.title("Зависимость времени MMSE от числа OFDM символов")
+plt.grid()
+plt.show()
